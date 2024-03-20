f629b8f3060fef6e864705f386df0659fce63ba4
Jasper Kooij <jasperkooij@gmail.com>
2024-03-20 14:17:26 -0500

    lets' try without override

--- src/builder-registry.ts
+++ src/builder-registry.ts
@@ -17,18 +17,5 @@ Builder.registerComponent(Counter, {
 
 Builder.registerComponent(BuilderImage, {
   name: 'Image', // The same name you used in Builder.io
-  override: true,
-  inputs: [
-    // Add any custom inputs if needed, repeating standard ones
-    { name: 'image', type: 'file' },
-    { name: 'backgroundPosition', type: 'string' },
-    { name: 'lazy', type: 'boolean' },
-    { name: 'fitContent', type: 'boolean' },
-    { name: 'aspectRatio', type: 'number' },
-    { name: 'lockAspectRatio', type: 'boolean' },
-    { name: 'height', type: 'number' },
-    { name: 'width', type: 'number' },
-    { name: 'altText', type: 'string' },
-    { name: 'className', type: 'string' }
-  ],
+
 });
